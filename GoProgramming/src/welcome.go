142406740d8ce457138460a7f5ca98d2853ea30b
P. Raveendra Reddy <raveendrapadala@users.noreply.github.com>
2016-12-13 04:56:27 +0530

    trial commit

---
+++ GoProgramming/src/welcome.go
@@ -0,0 +1,8 @@
+//Standalone executable program must start with main package declaration.main is the package you are creating.
+// if the package name is otherthan main then it means we are making a shared library/dynamic library
+package main
+//import keyword is how we include code from other packages to use with our program.
+import "fmt"
+//Go Programming language Specification: https://golang.org/ref/spec
+//For examples, Refer: https://gobyexample.com
+//To understand about packages refer, https://golang.org/pkg/
