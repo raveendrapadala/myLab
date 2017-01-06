a9dca8dc9b4263934226d5a60c65ad9f93d849c8
P. Raveendra Reddy <raveendrapadala@users.noreply.github.com>
2017-01-06 05:48:03 +0530

    updated with ascii character printing function
    A new function was introduced to print all the ascii characters.

--- GoProgramming/src/welcome.go
+++ GoProgramming/src/welcome.go
@@ -3,6 +3,23 @@
 package main
 //import keyword is how we include code from other packages to use with our program.
 import "fmt"
+
+/**
+ * This function prints the ascii table
+ *  Here %q represents character surrounded by single quotes
+**/
+func printASCIIValues()(){
+  fmt.Printf("Decimal,Binary,HexDecimal,character\n",)
+  for itrCount := 0; itrCount < 122; itrCount++  {
+	fmt.Printf("%d \t %b \t %x \t %q \n", itrCount, itrCount, itrCount, itrCount)
+  }
+}
+
+func main()(){
+  printASCIIValues()
+}
+
+
 //Go Programming language Specification: https://golang.org/ref/spec
 //For examples, Refer: https://gobyexample.com
 //To understand about packages refer, https://golang.org/pkg/
