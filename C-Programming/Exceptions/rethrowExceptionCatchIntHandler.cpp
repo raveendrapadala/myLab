96cbd5ad3d879ad045d6bf3c5f7b9ee1902c5eab
P. Raveendra Reddy <raveendrapadala@users.noreply.github.com>
2017-05-25 05:44:26 +0530

    Create rethrowExceptionCatchIntHandler.cpp
    basic version

---
+++ C-Programming/Exceptions/rethrowExceptionCatchIntHandler.cpp
@@ -0,0 +1,33 @@
+/*
+ *Temporary object is created only once, at the ‘throw’ statement in function f(). 
+ * But its storage duration lasts until the LAST handler being executed for the exception exits by any means other than a ‘throw;’ 
+ * In this example, it is until the handler in ‘fun’ exits.
+ */
+
+struct E {
+  E() { cout << " E() "; }
+  E(const E& rhs) { 
+    cout << " E(E&) "; 
+  }
+};
+
+void f() {
+  E  e;
+  throw e;
+}
+void fun() {
+  try {
+    f();
+  } catch (E  e) {
+    cout << "fun";
+    throw 1;
+  }
+}
+
+int main() {
+  try {
+    fun();
+  } catch (int i) {
+    cout << "main";
+  }
+}
