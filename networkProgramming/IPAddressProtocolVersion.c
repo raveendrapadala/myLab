7979181d79555bdc2da24fe6444552641fff4c25
P. Raveendra Reddy <raveendrapadala@users.noreply.github.com>
2017-11-30 10:29:37 +0530

    Update IPAddressProtocolVersion.c
    Modified as per corporate style.

--- networkProgramming/IPAddressProtocolVersion.c
+++ networkProgramming/IPAddressProtocolVersion.c
@@ -1,15 +1,34 @@
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
-static int
-ip_version(const char *src) {
-    char buf[16];
-    if (inet_pton(AF_INET, src, buf)) {
-        return 4;
-    } else if (inet_pton(AF_INET6, src, buf)) {
-        return 6;
+#define IPV4_ADDRESS    4
+#define IPV6_ADDRESS    6
+#define INVALID_IP_ADDRESS  -1
+
+/*  This function validates the protocol version of the given string(IP Address)
+ *  Input Params:
+ *    ipAddress -  IPV4Address given by user in string format.
+ *  Return value:
+ *    4 - if the user input string is IPV4 Address.
+ *    6 - If the user input string is IPV6 Address.
+ *   -1 - If the user input string is not a valid IP Address.
+ */
+int ip_version(const char *ipAddress) 
+{
+    char nwAddrStruct[16];
+    
+    if (inet_pton(AF_INET, ipAddress, nwAddrStruct)) 
+    {
+        return IPV4_ADDRESS;
     }
-    return -1;
+    else if (inet_pton(AF_INET6, ipAddress, nwAddrStruct)) 
+    {
+        return IPV6_ADDRESS;
+    }
+    else
+    {
+        return INVALID_IP_ADDRESS;
+    }
 }
 
 int
@@ -26,3 +45,11 @@ main(void ) {
     
     return 0;
 }
+
+#if 0
+127.0.0.1	 4
+2300:c100::1:4	 6
+fe80::ba2a:72ff:fefb:de4a	 6
+2300:100::1:4	 6
+raveendra	 -1
+#endif
