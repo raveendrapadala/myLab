03115013ebd70d63df3688259d1a5b6546073ebe
P. Raveendra Reddy <raveendrapadala@users.noreply.github.com>
2018-12-19 13:56:30 +0530

    Create lseek.c

---
+++ cProgramming/src/file_io/lseek.c
@@ -0,0 +1,15 @@
+/*
+* DESCRIPTION: Tests standard input to see whether it's capable of seeking 
+*/
+#include <unistd.h>
+
+int main (void)
+{
+  /* check whether stdin is able to seek or not? */
+  if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
+    printf("cannot seek\n");
+  else
+    printf("seek OK\n");
+  
+  exit(0);
+}
