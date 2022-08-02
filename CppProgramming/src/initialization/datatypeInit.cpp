2d96eeacbddba9b75d049aa53a7b64c7c3fe24be
P. Raveendra Reddy <raveendrapadala@users.noreply.github.com>
2022-08-02 12:24:36 +0530

    Update datatypeInit.cpp
    updated....

--- CppProgramming/src/initialization/datatypeInit.cpp
+++ CppProgramming/src/initialization/datatypeInit.cpp
@@ -1,29 +1,39 @@
-// initialization of variables
-#include <iostream>
+/* Uniform initialization is a feature in C++ 11 that allows the usage of a consistent syntax to initialize variables and objects ranging from primitive type to aggregates. In other words, it introduces brace-initialization that uses braces ({}) to enclose initializer values. The syntax is as follows:
+type var_name{arg1, arg2, ....arg n}*/
 
-//string is not a fundamental datatype, means we need to include an 
-//additional header file in our source code: <string> and have access 
-//to the std namespace
-#include <string>
+#include <iostream>// initialization of variables
+#include <string> //string is not a fundamental datatype, means we need to include an additional header file in our source code: <string> and have access to the std namespace
 using namespace std;
-
+//Namespace must be out of main
+namespace myNamespace
+{
+  int intVar = 5;
+  double doubVar = 3.142;
+}
+void integerInitialization(void);
 int main()
 {
- //Integers Initialization
- int a=5; // initial value = 5
- // constructor initialization, is done by enclosing the initial value
- //between parentheses (()):
- int b(2);
- int result; // initial value undetermined
- cout<< a << b << result << endl;
- 
- //Strings Initialization
- string mystring;
- mystring = "This is the initial string content";
- cout << mystring << endl;
- mystring = "This is a different string content";
- cout << mystring << endl;
- 
- //Gracefull shutdown of any process is must for valgrind
- return 0;
+    integerInitialization();
+    //Strings Initialization, parameterized Initialization
+    string conString ("Constructor Initialized string");
+    cout << "paramConstInit=" << conString << endl;
+    string uniformInitString {"Uniform Initialized string"};
+    cout << uniformInitString << endl;
+    cout << "\nintVar=" << myNamespace::intVar << "\ndoubvar=" << myNamespace::doubVar<< endl;
+    //Gracefull shutdown of any process is must for valgrind
+    return 0;
  }
+
+void integerInitialization(void)
+{
+ //Integers Initialization
+    int a=5;
+    /* constructor initialization, is done by enclosing the initial value
+    between parentheses:*/
+    int b(2);
+    int result{3}; // Uniform Initializatio introduced in C++ 11
+    int copyConst{result};
+    //error: expected unqualified-id before ‘using’
+    //int using = 10;
+    cout<< "Init Var a=" << a << "\nConstructor Var b=" << b << "\nresult=" << result << "\ncopyConstFromResult" << copyConst << endl;
+}
